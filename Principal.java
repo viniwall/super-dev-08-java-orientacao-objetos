fa9a525f8789601f816394ce3b63719a7b94fe44
Vinicius Imhof Waldrigues <vinicius.waldrigues@unifebe.edu.br>
2026-07-04 12:44:22 -0300

    Perdão professor querido

--- Principal.java
+++ Principal.java
@@ -1,7 +1,5 @@
 public class Principal {
     public static void main(String[] args) {
-        // JogoExecutor executor = new JogoExecutor();
-        // FabricanteExecutor executor = new FabricanteExecutor();
         CarroExecutor executor = new CarroExecutor();
         executor.executar();
     }
