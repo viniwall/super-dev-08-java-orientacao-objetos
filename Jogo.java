632896e495079de69badc7adfeb99447b8a65574
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2026-07-04 10:20:22 -0300

    Aula 2026-07-04

---
+++ Jogo.java
@@ -0,0 +1,13 @@
+// super-dev-08-java-orientacao-objetos
+
+public class Jogo {
+    // Atributos (características)
+    public String nome;
+
+    public double preco;
+
+    public int parcelamentoDisponivel;
+
+    public double valorParcela;
+}
+
---
+++ Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        // Classe nomeObjeto = new Classe
+        JogoExecutor executor = new JogoExecutor();
+        executor.executar();
+    }
+}
